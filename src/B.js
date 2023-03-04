b4c727f4dc0aadacba6da7f36b1d4b52851c942b
lohan <alstjd7540@naver.com>
2023-03-04 19:57:15 +0900

    React.memo()사용

--- src/B.js
+++ src/B.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
 
-const Message = ({ message }) => {
+const Message = React.memo(({ message }) => {
   return <p>{message}</p>
-}
+})
 
-const ListItem = ({ post }) => {
+const ListItem = React.memo(({ post }) => {
   return (
     <li key={post.id}>
       <p>{post.title}</p>
     </li>
   )
-}
+})
 
-const List = ({posts}) => {
+const List = React.memo(({posts}) => {
   return (
     <ul>
       {posts.map(post => (
@@ -21,7 +21,7 @@ const List = ({posts}) => {
       ))}
     </ul>
   )
-}
+})
 
 const B = ({ message, posts }) => {
 
